309b2a773f6150b129cb8fa012f48b68ff9734b5
minhtri-dev <minhtritran.dev@gmail.com>
2025-06-14 20:31:34 +1000

    Update ListingCriteria to allow for greater than or equal filtering on bedrooms

--- backend/src/services/database/listing.service.ts
+++ backend/src/services/database/listing.service.ts
@@ -3,7 +3,7 @@ import { ListingModel } from '@/models/listings.model'
 interface ListingCriteria {
   "address.market": string;
   property_type?: string;
-  bedrooms?: number;
+  bedrooms?: number | { $gte: number };
 }
 
 export const fetchAllListings = async (limit = 10, page = 1) => {
@@ -14,7 +14,13 @@ export const fetchListingById = async (id: string) => {
   return await ListingModel.findById(id)
 }
 
-export const searchListings = async (location: string, property_type?: string, bedrooms?: number, page = 1, limit = 10) => {
+export const searchListings = async (
+  location: string, 
+  property_type?: string, 
+  bedrooms?: number, 
+  page = 1, 
+  limit = 10
+) => {
   const criteria: ListingCriteria = {
     "address.market": location,
   }
@@ -22,7 +28,11 @@ export const searchListings = async (location: string, property_type?: string, b
     criteria.property_type = property_type
   }
   if (bedrooms !== undefined && !isNaN(bedrooms)) {
-    criteria.bedrooms = bedrooms
+    if (bedrooms === 4) {
+      criteria.bedrooms = { $gte: 4 }
+    } else {
+      criteria.bedrooms = bedrooms
+    }
   }
   return await ListingModel.paginate(criteria, { limit, page })
 }
